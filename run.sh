9be5d31195e3969b7c145fee3dcb1ea58e641f4c
bgallois <benjamin@gallois.cc>
2022-06-28 14:00:00 +0200

    [feat] add ci

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
-pyside2-rcc src/resources.qrc -o src/rc_ressources.py
-pyside2-uic src/fastanalyzer.ui > src/ui_fastanalyzer.py
-pyside2-uic src/plot.ui > src/ui_plot.py
-pyside2-uic src/plot_settings.ui > src/ui_plot_settings.py
-pyside2-uic src/data_calc.ui > src/ui_data_calc.py
+./build.sh
 python src/fastanalyzer.py
